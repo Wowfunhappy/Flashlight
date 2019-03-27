a1d3ae75e6bb76a83f549d70ebc164e1412dcdc4
Jonathan Alland <jonathan@ideasonpurpose.com>
2019-03-27 13:50:42 -0400

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Jonathan Alland on 3/27/19.
+//  Copyright © 2019 Jonathan Alland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
